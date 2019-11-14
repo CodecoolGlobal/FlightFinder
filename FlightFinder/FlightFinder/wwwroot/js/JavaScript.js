8610cf262f3644ccee0024ab695a2b6342266838
kruppatomi <hungaryca@gmail.com>
2019-11-14 21:19:30 +0100

    autofill and airports bind

---
+++ FlightFinder/FlightFinder/wwwroot/js/JavaScript.js
@@ -0,0 +1,109 @@
+﻿function getcountries() {
+var data = null;
+
+var xhr = new XMLHttpRequest();
+xhr.withCredentials = true;
+
+xhr.addEventListener("readystatechange", function () {
+    if (this.readyState === this.DONE) {
+        
+        var result = JSON.parse(this.responseText.substring(13).slice(0, -1));
+    autocomplite(result, "#countries");
+    autocomplite(result, "#countries2");
+
+    }
+});
+
+xhr.open("GET", "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/reference/v1.0/countries/hu-HU");
+xhr.setRequestHeader("x-rapidapi-host", "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com");
+xhr.setRequestHeader("x-rapidapi-key", "ce1241679dmshdbe323b73c0dde6p1f7e5ejsn386ae855ecfa");
+
+xhr.send(data);
+
+}
+
+function getairports(country, pickerid) {
+    var data = null;
+
+    var xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+
+    xhr.addEventListener("readystatechange", function () {
+        if (this.readyState === this.DONE) {
+            console.log(this.responseText);
+            var result = JSON.parse(this.responseText.substring(10).slice(0, -1));
+                var picker = document.getElementById(pickerid)
+            for (let i in result) {
+                var node = document.createElement("option");
+                node.textContent = result[i]['PlaceId'];
+                picker.appendChild(node);
+            }
+        }
+    });
+    
+
+    xhr.open("GET", "https://"+`skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/autosuggest/v1.0/HU/HUF/hu-HU/?query=${country}`);
+    xhr.setRequestHeader("x-rapidapi-host", "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com");
+    xhr.setRequestHeader("x-rapidapi-key", "ce1241679dmshdbe323b73c0dde6p1f7e5ejsn386ae855ecfa");
+
+    xhr.send(data);
+}
+
+getcountries();
+
+
+function autocomplite(json, idstring) {
+    var options = {
+
+        data: json,
+
+        getValue: "Name",
+
+        list: {
+            match: {
+                enabled: true
+            }
+        },
+
+        theme: "square"
+    };
+
+    $(idstring).easyAutocomplete(options);
+}
+
+var from = document.getElementById("countries");
+document.onkeydown = checkKey;
+
+var to = document.getElementById("countries2")
+
+function checkKey(e) {
+
+    e = e || window.event;
+
+    if (e.keyCode == '13') {
+        removeChilds();
+        getairports(encode_utf8(from.value), "selectpicker" );
+        getairports(encode_utf8(to.value), "selectpicker2")
+    }
+
+}
+function removeChilds() {
+    var list = document.getElementById("selectpicker");
+    var list2 = document.getElementById("selectpicker2");
+
+    while (list.hasChildNodes() && list2.hasChildNodes()) {
+        list.removeChild(list.firstChild);
+        list2.removeChild(list2.firstChild);
+    }
+    while (list2.hasChildNodes()) {
+        list2.removeChild(list2.firstChild);
+    }
+}
+
+function encode_utf8(s) {
+    return encodeURIComponent(s);
+}
+
+
+
+
--- FlightFinder/FlightFinder/wwwroot/js/site.js
+++ FlightFinder/FlightFinder/wwwroot/js/site.js
@@ -8,8 +8,8 @@ var sessionKey;
 
 //get your date from the input fields
 function getData() {
-    var departure = document.getElementById("Departure").value;
-    var destination = document.getElementById("Destination").value;
+    var departure = String(document.getElementById("selectpicker").value);
+    var destination = String(document.getElementById("selectpicker2").value);
     var startDate = document.getElementById("start").value;
     var endDate = document.getElementById("end").value;
 
